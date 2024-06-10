e225fd685db0ed19798aab43d64a93e42680f64b
Jagad R R <jagadraya388@gmail.com>
2024-06-10 20:18:35 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+	res.json({
+		message: 'Hello World'
+	})
+})
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`)
+})
